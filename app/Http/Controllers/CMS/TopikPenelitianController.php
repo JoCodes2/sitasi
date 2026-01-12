f4a7337c945cefb331559debf16b97af912ec8f6
divta-suryawan <gitdivta@gmail.com>
2026-01-12 14:54:18 +0800

    create api topik penelitian

---
+++ app/Http/Controllers/CMS/TopikPenelitianController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers\CMS;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\TopikPenelitianRequest;
+use App\Repositories\TopikPenelitianRepositories;
+use Illuminate\Http\Request;
+
+class TopikPenelitianController extends Controller
+{
+    protected $topikPenelitian;
+
+    public function __construct(TopikPenelitianRepositories $topikPenelitian)
+    {
+        $this->topikPenelitian = $topikPenelitian;
+    }
+    public function getAllData()
+    {
+        return $this->topikPenelitian->getAllData();
+    }
+    public function createData(TopikPenelitianRequest $request)
+    {
+        return $this->topikPenelitian->createData($request);
+    }
+    public function getDataById($id)
+    {
+        return $this->topikPenelitian->getDataById($id);
+    }
+
+    public function updateData(TopikPenelitianRequest $request, $id)
+    {
+        return $this->topikPenelitian->updateData($request, $id);
+    }
+    public function deleteData($id)
+    {
+        return $this->topikPenelitian->deleteData($id);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,20 @@
 <?php
 
+use App\Http\Controllers\CMS\TopikPenelitianController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+// route api
+Route::prefix('sitasi')->group(function () {
+    Route::prefix('topik')->controller(TopikPenelitianController::class)->group(function () {
+        Route::get('/', 'getAllData');
+        Route::post('/create', 'createData');
+        Route::get('/get/{id}', 'getDataById');
+        Route::post('/update/{id}', 'updateData');
+        Route::delete('/delete/{id}', 'deleteData');
+    });
 });
